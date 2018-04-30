c9740b81b2fa329490c776667f1a44903d516cdd
chaitea <chaitea@umich.edu>
2018-04-29 21:36:31 -0400

    asdf

--- test.py
+++ test.py
@@ -2,8 +2,7 @@ import urllib.request, json
 from yahoo_finance import Share
 
 
-with urllib.request.urlopen("https://query2.finance.yahoo.com/v10/finance/quoteSummary/{}?modules=assetProfile".format("NVDA")) as url:
+with urllib.request.urlopen("https://query2.finance.yahoo.com/v10/finance/quoteSummary/{}?modules=balanceSheetHistory".format("NVDA")) as url:
 	data = json.loads(url.read().decode())
 	jsondata = data["quoteSummary"]["result"]
-	for item in data
-		print(item)
+	print(data)
